e1cd6dbd359449800cedfe870e2385b363dc3b92
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-20 13:38:54 -0600

    Seguimos declarando los métodos

--- Estructura/main.c
+++ Estructura/main.c
@@ -4,18 +4,36 @@
 
 struct computer{
     char entrada[10];
+    char salida[10];
     char marca[10];
     char modelo[10];
     int memoria;
 
-}
+};
+
+void readData(struct computer c);
+
+struct computer create();
 //Uso de typedef unsigned long long int Var; donde var es lo que puede variar
 int main()
 {
 
-    //computador c1;
     printf("Hello world!\n");
+     struct computer c1;
 
+     c1.entrada = "Teclado";
+     c1.marca = "Hewlett packard";
+     c1.modelo = "54as";
+     c1.salida = "Pantalla";
+     c1.memoria = 1322;
+
+     struct computer c2{
+        "Mouse",
+        "Bocinas",
+        "Asus",
+        "54adf548",
+        1544
+     }
     return 0;
 }
 
