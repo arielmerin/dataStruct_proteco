afd22b709c32991e1ca78c9d01707ed162caa872
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-20 13:13:00 -0600

    Este tiene el primer documento

---
+++ Estructura/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#Uso de typedef unsigned long long int Var; donde var es lo que puede variar
+int main()
+{
+    typedef struct computador{
+        char entrada[10];
+        char salida[10];
+
+
+    }
+    printf("Hello world!\n");
+    return 0;
+}
