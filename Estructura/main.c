7338caf8d3ad4d0e085cc47fbf19bf221de3e6cc
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-20 13:30:48 -0600

    Este tiene los primeros archivos sobre como declarara estructuras

--- Estructura/main.c
+++ Estructura/main.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 
 
+struct computer{
+    char entrada[10];
+    char marca[10];
+    char modelo[10];
+    int memoria;
 
-#Uso de typedef unsigned long long int Var; donde var es lo que puede variar
+}
+//Uso de typedef unsigned long long int Var; donde var es lo que puede variar
 int main()
 {
-    typedef struct computador{
-        char entrada[10];
-        char salida[10];
 
-
-    }
+    //computador c1;
     printf("Hello world!\n");
+
     return 0;
 }
+
