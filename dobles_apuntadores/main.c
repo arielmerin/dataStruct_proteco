18a19f7bae9b6bad0a1af4e7518c5fb0b0d0cbed
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-21 15:12:27 -0600

    Pasos para hacer un arreglo dinámico

---
+++ dobles_apuntadores/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Apuntador doble , arreglos sobes!\n");
+
+
+    //Declarar variables
+
+    //Reservar memoria
+
+    //Validar el espacio
+
+
+    //Procesamiento de datos
+
+
+    //Librear memoria (free)
+    return 0;
+}
+
+
+/*
+Cómo hacer un apuntador doble o un apuntador a un apuntador
+Hay qye gacer dis oasis y luefo cada uno de esos y así recursivamente (esto es un apuntador pero en una matriz de 2*2)
+
+
+*/
