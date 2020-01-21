12ccd991822587978c177a00eb7da983abfa9712
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-21 12:58:04 -0600

    Tiene el primer código con malloc, calloc, realloc, free

---
+++ funciones_enC/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+
+/*Malloc -> reserva el espacio en bytes
+    cuando solo tenemos que reservar y en seguida podemos asignar
+calloc -> reserva nmero de elemento sy tambien los limpia (com con un 0 o null)
+    Nos conviene cuando tenemos una agenda teleófinca,
+    ejemplo si lo hiciéramos con malloc tendría basura
+realloc -> cambair la asignación (tamaño)
+free solo sirve para liberar
+
+
+
+*/
