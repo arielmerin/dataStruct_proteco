d953621eb718450dc9207b0f3f434be7005ab43e
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-21 15:40:44 -0600

    este tiene cmpleto el código de cómo poner el arreglo bidimensional

--- dobles_apuntadores/main.c
+++ dobles_apuntadores/main.c
@@ -8,11 +8,15 @@ int main()
 
     //Declarar variables
     int raw, col;
-
     int** matrix = NULL;
 
+    printf("Ingresa los renglones de la matriz: ");
+    scanf("%d", &raw);
+    printf("Ingresa el número de columnas: ", &col);
+    scanf("%d", &col);
+
     //Reservar memoria
-    matrix = (int**)malloc(raw*sizeof(int*))
+    matrix = (int**)malloc(raw*sizeof(int*));
     /*
     Hacer un ciclo que dependa del número de renglones
     porque es uno por uno
@@ -23,14 +27,47 @@ int main()
         Esta es otra manera de hacer lo que está en la linea anterior
         *(matrix + i) = (int*)malloc(col*sizeof(int));
         */
-
     }
 
     //Validar el espacio
+    if(matrix == NULL){
+        printf("Errorm no pudimos reservar el espacio en la memoria");
+        return -1;
+    }
 
 
     //Procesamiento de datos
+    /**
+        En este caso estamos recorriendo un arreglo de manera bidimensional con ciclos anidados
+    */
+    for(int i = 0;  i < raw; i++){
 
+        for(int j = 0; j < col; j++){
+            printf("Ingresa el elemnto %i, %i :  ", i +1 , j+1);
+            /**
+            Recordar que matriz ya es una dirección de memoria, con i puedo moverme en los renglones y luego le sumamos
+            j que sirve para moverse entre las columnas
+            */
+            scanf("%i", *(matrix + i) + j);
+
+        }
+    }
+
+    for(int i = 0;  i < raw; i++){
+
+        for(int j = 0; j < col; j++){
+        /**
+        Nos olvidamos de la flecha porque esto no es una estructura
+        */
+            printf("Mostrando el elemento alojado en: %i rengln y %i columna = %i \n", i, j, *(*(matrix + i) + j) );
+        }
+    }
+
+    for(int i = 0; i < raw; i++){
+        free(matrix[i]);
+    }
+
+    free(matrix);
 
     //Librear memoria (free)
     return 0;
