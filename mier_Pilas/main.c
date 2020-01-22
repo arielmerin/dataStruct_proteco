820e257b921167ee0cf34509ed2e649da45fb454
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-22 13:09:31 -0600

    Tiene más cambios y agregando la explicaciónd el profe

--- mier_Pilas/main.c
+++ mier_Pilas/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node{
+    int content;
+    struct Node* sig;
+
+
+};
+
+struct Node createNode{
+
+
+    return ;
+}
+
+
 int main()
 {
     printf("Hello world!\n");
     return 0;
 }
+/**
+Confort crowd
+this is in my opinion a great opotunity to meke an awsome program or that and i dont really feel right begin in this place
+Hay muchas manera de programar las estructuras de datos, una para los nodos y una para la
+estructura de datos en general
+
+
+*/
