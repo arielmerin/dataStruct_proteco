8136d5cfd0a8a742fe4fd70b26daa87888d5e994
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-23 14:12:48 -0600

    Prototipos de las funciones

--- juev_lista/main.c
+++ juev_lista/main.c
@@ -14,9 +14,21 @@ typedef struct Nodo {
     struct Nodo* siguiente;
 } Nodo;
 
-//Funciones de nodo
-Nodo* crearNodo(int dato
+typedef struct List
+{
+    Nodo* front;
+    Nodo* back;
+    int size;
+}List;
 
+List* create_list();
+bool isEmptyList(List* l);
+void insertFront(List* l, int data);
+void insertBack(List* l, int data);
+int revomeFront(List* l);
+int revomeBack(List* l);
+int removeat(List* l, int posicion);
+int lookAt(List* l, int posicion)
 //Definiendo funciones de nodos
 Nodo * crearNodo(int dato) {
     Nodo *nuevo= (Nodo*)malloc(sizeof(Nodo));
@@ -32,3 +44,6 @@ Nodo * crearNodo(int dato) {
 
 
 #endif
+
+/**
+ * Este tipo de isEmtpyList() recibe sólo un apuntador y no dos tipos de aputnadores
