2f043a939c4acab7cd3cf54413c5683d4bf453ed
arielmerin <arielmerino@ciencias.unam.mx>
2020-01-23 14:01:42 -0600

    Con los cambios base de los nodos ocupados en otro lugar

--- juev_lista/main.c
+++ juev_lista/main.c
@@ -15,7 +15,20 @@ typedef struct Nodo {
 } Nodo;
 
 //Funciones de nodo
-Nodo* crearNodo(int dato);
+Nodo* crearNodo(int dato
+
+//Definiendo funciones de nodos
+Nodo * crearNodo(int dato) {
+    Nodo *nuevo= (Nodo*)malloc(sizeof(Nodo));
+    if(nuevo==NULL) {
+        printf("Error al crear nodo");
+        return NULL;
+    }
+    nuevo->dato=dato;
+    nuevo->siguiente=NULL;
+    return nuevo;
+}
+
 
 
 #endif
